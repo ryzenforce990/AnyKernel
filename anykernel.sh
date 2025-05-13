58adaa18c5d2cf673ad4c2feb1a6b9d4637a0749
SuperXorn <151366950+ryzenforce990@users.noreply.github.com>
2025-05-13 15:49:01 +0700

    [TMP]

--- anykernel.sh
+++ anykernel.sh
@@ -4,7 +4,7 @@
 ### AnyKernel setup
 # global properties
 properties() { '
-kernel.string=Kernel by aryannn999 @ xda-developers
+kernel.string=Kernel by superxorn @ xda-developers
 do.devicecheck=1
 do.modules=0
 do.systemless=1
